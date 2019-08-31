70df225b6b479bf18135e347ca779bfdcf32b2ff
Cristian371 <czend371@gmail.com>
2019-08-30 22:07:17 -0500

    Updates

---
+++ precache-manifest.50ff8906d083a2e389fdcb9903211306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b176d39c372f898cbd2eb2a1e48ae63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9542a1d1764b687518ee",
+    "url": "/static/css/main.bb8460d5.chunk.css"
+  },
+  {
+    "revision": "cb334384f511159f4b99",
+    "url": "/static/js/2.35343ec5.chunk.js"
+  },
+  {
+    "revision": "9542a1d1764b687518ee",
+    "url": "/static/js/main.c663e870.chunk.js"
+  },
+  {
+    "revision": "015c42324f48d34e53b4",
+    "url": "/static/js/runtime~main.92fb9317.js"
+  }
+]);
